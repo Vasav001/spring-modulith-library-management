15412696a6e17885891d0890e66d8c2a79b9ebf2
Vasav Rastogi <vasavrastogi@gmail.com>
2026-06-05 22:16:21 +0530

    first commit

---
+++ src/test/java/com/vasav/springmodulithlibrarymanagement/SpringModulithLibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasav.springmodulithlibrarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithLibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
