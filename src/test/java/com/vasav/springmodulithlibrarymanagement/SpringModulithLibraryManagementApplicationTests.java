46810ae1ea463f1e21d7b0b69790959ef506d584
Vasav Rastogi <vasavrastogi@gmail.com>
2026-06-07 20:06:06 +0530

    add initial infra configuration
    add flyway modulith event publication table; add initial prometheus config

--- src/test/java/com/vasav/springmodulithlibrarymanagement/SpringModulithLibraryManagementApplicationTests.java
+++ src/test/java/com/vasav/springmodulithlibrarymanagement/SpringModulithLibraryManagementApplicationTests.java
@@ -2,6 +2,7 @@ package com.vasav.springmodulithlibrarymanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
 
 @SpringBootTest
 class SpringModulithLibraryManagementApplicationTests {
@@ -10,4 +11,8 @@ class SpringModulithLibraryManagementApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void verifyModularStructure() {
+		ApplicationModules.of(SpringModulithLibraryManagementApplication.class).verify();
+	}
 }
