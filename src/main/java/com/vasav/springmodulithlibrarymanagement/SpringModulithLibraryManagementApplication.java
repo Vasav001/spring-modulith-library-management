3559e0087c3b2c89de8808cf18e300a02400b504
Vasav Rastogi <vasavrastogi@gmail.com>
2026-08-07 17:58:37 +0530

    switch to springboot v4.1.0, add depencencies and docker compose for postgres

--- src/main/java/com/vasav/springmodulithlibrarymanagement/SpringModulithLibraryManagementApplication.java
+++ src/main/java/com/vasav/springmodulithlibrarymanagement/SpringModulithLibraryManagementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringModulithLibraryManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringModulithLibraryManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringModulithLibraryManagementApplication.class, args);
+    }
 
 }
